6460f88740861d6d5d455a45b823bd13a02128e3
Nitin Mohan <nitin@rightscale.com>
2012-05-22 19:11:32 +0000

    Added templatesto generate pages for cookbooks and lwrp

--- lib/templates/default/fulldoc/html/setup.rb
+++ lib/templates/default/fulldoc/html/setup.rb
@@ -5,27 +5,19 @@ def init
   # Generate page for Chef
   serialize(YARD::CodeObjects::Chef::CHEF_NAMESPACE)
 
-  @cookbook_elements = Registry.all(:cookbookelement)
-  @cookbook_elements.find_all.each do |child|
-    serialize(child)
-  end
+  #@cookbook_elements = Registry.all(:cookbookelement)
+  #@cookbook_elements.find_all.each do |child|
+    #serialize(child)
+  #end
   
   #TODO: need to generate lwrp.html
   @cookbooks = Registry.all(:cookbookname).sort_by{|cookbook| cookbook.name.to_s}
+  @cookbooks.each do |cookbook|
+    serialize(cookbook)
+  end
   #generate_full_list(@cookbooks, "Cookbook", "cookbooks")
   
   #TODO: Don't understand why Registry.all() prints the same item thrice. Look into that
-  @actions = Registry.all(:action).uniq{|action| action.name.to_s}
-  @actions = @actions.sort_by{|action| action.name.to_s}
-  generate_full_list(@actions, "Action", "actions")
-
-  @attributes = Registry.all(:attribute).uniq{|attribute| attribute.name.to_s}
-  @attributes = @attributes.sort_by{|attribute| attribute.name.to_s}
-  generate_full_list(@attributes, "Attribute", "attributes")
-
-  @definitions = Registry.all(:definition).uniq{|define| define.name.to_s}
-  @definitions = @definitions.sort_by{|define| define.name.to_s}
-  generate_full_list(@definitions, "Definition", "definitions")
 
   generate_lwrp
   
@@ -59,7 +51,30 @@ def items_of_type(cookbook, type)
 end
 
 def generate_lwrp
+  #asset(url_for("lwrp"), erb(:lwrp))
   asset("lwrp.html", erb(:lwrp))
+end
+
+# Called by menu_lists in layout/html/setup.rb by default
+def generate_actions_list
+  #TODO: Don't understand why Registry.all() prints the same item thrice. Look into that
+  @actions = Registry.all(:action).uniq{|action| action.name.to_s}
+  @actions = @actions.sort_by{|action| action.name.to_s}
+  generate_full_list(@actions, "Action", "actions")
+end
+
+# Called by menu_lists in layout/html/setup.rb by default
+def generate_attributes_list
+  @attributes = Registry.all(:attribute).uniq{|attribute| attribute.name.to_s}
+  @attributes = @attributes.sort_by{|attribute| attribute.name.to_s}
+  generate_full_list(@attributes, "Attribute", "attributes")
+end
+
+# Called by menu_lists in layout/html/setup.rb by default
+def generate_definitions_list
+  @definitions = Registry.all(:definition).uniq{|define| define.name.to_s}
+  @definitions = @definitions.sort_by{|define| define.name.to_s}
+  generate_full_list(@definitions, "Definition", "definitions")
 end
 
 def generate_full_list(objects, title, type)
