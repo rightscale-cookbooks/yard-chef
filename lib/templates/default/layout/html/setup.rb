f420d36c5e452cd79f4c21aaba28f75d259add02
Nitin Mohan <nitin@rightscale.com>
2012-05-22 19:10:01 +0000

    Added custom yard-chef menus

---
+++ lib/templates/default/layout/html/setup.rb
@@ -0,0 +1,13 @@
+def init
+  super
+end
+
+# Add yard-chef specific menus
+# Methods generate_#{type}_list  must be defined in fulldoc/html/setup.rb
+def menu_lists
+[ { :type => 'actions', :title=> 'Actions', :search_title => 'Action List'},
+  { :type => 'attributes', :title => 'Attributes', :search_title => 'Attribute List'},
+  { :type => 'class', :title => 'Classes', :search_title => 'Class List' },
+  { :type => 'method', :title => 'Methods', :search_title => 'Method List' },
+  { :type => 'file', :title => 'Files', :search_title => 'File List' } ]
+end
