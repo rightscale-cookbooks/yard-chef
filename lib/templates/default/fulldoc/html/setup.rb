acbe683d740924a4d3a1f11ef91296690bd54322
Nitin Mohan <nitin@rightscale.com>
2012-05-25 00:35:57 +0000

    Added cookbook list menu.

--- lib/templates/default/fulldoc/html/setup.rb
+++ lib/templates/default/fulldoc/html/setup.rb
@@ -3,8 +3,8 @@ def init
   @cookbooks = Registry.all(:cookbookname).sort_by{|cookbook| cookbook.name.to_s}
 
   # Generate page for Chef
-  serialize(YARD::CodeObjects::Chef::CHEF_NAMESPACE)
-  serialize(YARD::CodeObjects::Chef::LWRP_NAMESPACE)
+  serialize(YARD::CodeObjects::Chef::CHEF)
+  serialize(YARD::CodeObjects::Chef::LWRP)
   @cookbooks.each do |cookbook|
     serialize(cookbook)
   end
@@ -13,7 +13,7 @@ end
 # Called by menu_lists in layout/html/setup.rb by default
 def generate_actions_list
   #TODO: Don't understand why Registry.all() prints the same item thrice. Look into that
-  @actions = Registry.all(:action).uniq{|action| action.name.to_s}
+  @actions = YARD::Registry.all(:action).uniq{|action| action.name.to_s}
   @actions = @actions.sort_by{|action| action.name.to_s}
   generate_full_list(@actions, "Action", "actions")
   #@actions.each do |action|
@@ -23,16 +23,19 @@ end
 
 # Called by menu_lists in layout/html/setup.rb by default
 def generate_attributes_list
-  @attributes = Registry.all(:attribute).uniq{|attribute| attribute.name.to_s}
-  @attributes = @attributes.sort_by{|attribute| attribute.name.to_s}
+  @attributes = YARD::Registry.all(:attribute).uniq.sort_by{|attribute| attribute.name.to_s}
   generate_full_list(@attributes, "Attribute", "attributes")
 end
 
 # Called by menu_lists in layout/html/setup.rb by default
 def generate_definitions_list
-  @definitions = Registry.all(:definition).uniq{|define| define.name.to_s}
-  @definitions = @definitions.sort_by{|define| define.name.to_s}
+  @definitions = YARD::Registry.all(:definition).uniq.sort_by{|define| define.name.to_s}
   generate_full_list(@definitions, "Definition", "definitions")
+end
+
+def generate_cookbooks_list
+  @cookbooks = YARD::Registry.all(:cookbook).sort_by{|cookbook| cookbook.name.to_s}
+  generate_full_list(@cookbooks, "Cookbooks", "cookbooks")
 end
 
 def generate_full_list(objects, title, type)
@@ -42,6 +45,6 @@ def generate_full_list(objects, title, type)
   generate_list_contents
 end
 
-def generate_list_content
+def generate_list_contents
   asset(url_for_list(@list_type), erb(:full_list))
 end
