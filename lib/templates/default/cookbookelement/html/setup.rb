9ca55c1819594050dacef5900fa5e8007996408f
Nitin Mohan <nitin@rightscale.com>
2012-05-25 00:39:14 +0000

    Moved the erbs and setup.rb for LWRP to cookbookelement.

--- lib/templates/default/cookbookelement/html/setup.rb
+++ lib/templates/default/cookbookelement/html/setup.rb
@@ -1,3 +1,21 @@
 def init
   super
+  @cookbooks = YARD::Registry.all(:cookbook).sort_by {|cookbook| cookbook.name.to_s}
+  @actions = YARD::Registry.all(:action).uniq.sort_by {|action| action.namespace.to_s}
+  @attributes = YARD::Registry.all(:attribute).uniq.sort_by {|attrib| attrib.namespace.to_s}
+  sections.push :lwrp
+end
+
+
+# Returns items and unique namespaces of a particular cookbook
+def get_items_of_cookbook(item_list, cookbook)
+  list = []
+  namespaces = []
+  item_list.each do |item|
+    if item.parent.namespace == cookbook
+      list.push(item)
+      namespaces.push(item.namespace)
+    end
+  end
+  return list, namespaces.uniq {|name| name.to_s}
 end
