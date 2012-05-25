7d41668628395844200dcda62c8ed280bca7edbe
Nitin Mohan <nitin@rightscale.com>
2012-05-25 00:41:22 +0000

    Added cookbook list menu to the output

--- lib/templates/default/layout/html/setup.rb
+++ lib/templates/default/layout/html/setup.rb
@@ -5,7 +5,8 @@ end
 # Add yard-chef specific menus
 # Methods generate_#{type}_list  must be defined in fulldoc/html/setup.rb
 def menu_lists
-[ { :type => 'actions', :title=> 'Actions', :search_title => 'Action List'},
+[ { :type => 'cookbooks', :title => 'Cookbooks', :search_title => 'Cookbook List'},
+  { :type => 'actions', :title => 'Actions', :search_title => 'Action List'},
   { :type => 'attributes', :title => 'Attributes', :search_title => 'Attribute List'},
   { :type => 'class', :title => 'Classes', :search_title => 'Class List' },
   { :type => 'method', :title => 'Methods', :search_title => 'Method List' },
