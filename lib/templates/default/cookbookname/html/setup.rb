6ceceebdec8a8ee3c8107a72567a19f705dc7133
Nitin Mohan <nitin@rightscale.com>
2012-05-25 00:40:23 +0000

    Removed cookbookname from templates. No need for it.

--- lib/templates/default/cookbookname/html/setup.rb
+++
@@ -1,7 +0,0 @@
-include T('default/class/html')
-
-#def generate_actions
-  #@actions = Registry.all(:action).uniq{|action| action.name.to_s}
-  #@actions = @actions.sort_by{|action| action.name.to_s}
-  #return @actions
-#end
