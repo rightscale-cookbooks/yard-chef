27ac221f7e7c071c6cf4817752d5d4f7818126fd
Nitin Mohan <nitin@rightscale.com>
2012-05-31 16:15:12 +0000

    Added Cookbook list.

--- lib/templates/default/layout/html/setup.rb
+++ lib/templates/default/layout/html/setup.rb
@@ -12,3 +12,20 @@ def menu_lists
   { :type => 'method', :title => 'Methods', :search_title => 'Method List' },
   { :type => 'file', :title => 'Files', :search_title => 'File List' } ]
 end
+
+def format_object_type(object)
+  case object.type
+  when :class
+    if object.name.to_s == "RightScale"
+      return "Cookbook"
+    else
+      object.is_exception? ? "Exception" : "Class"
+    end
+  else
+    object.type.to_s.capitalize
+  end
+end
+
+def stylesheets
+  super + %w(css/common.css)
+end
