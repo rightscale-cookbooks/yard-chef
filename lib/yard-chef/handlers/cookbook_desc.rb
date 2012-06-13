8df24709382b84282a6485917d332b5544c22dde
Nitin Mohan <nitin@rightscale.com>
2012-06-13 23:08:35 +0000

    Handler to get description and version information from metadata.rb

---
+++ lib/yard-chef/handlers/cookbook_desc.rb
@@ -0,0 +1,45 @@
+# Copyright (c) 2012 RightScale, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# 'Software'), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+require 'yard'
+
+module YARD::Handlers
+  module Chef
+    class CookbookDescHandler < YARD::Handlers::Ruby::Base
+      include YARD::CodeObjects::Chef
+      handles method_call(:description)
+      handles method_call(:version)
+
+      def process
+        path_arr = parser.file.to_s.split('/')
+        cookbook_path = path_arr.slice(path_arr.index("cookbooks"), path_arr.size)
+        cookbook_obj = YARD::Registry.resolve(:root, "#{@@CHEF}::#{cookbook_path[1]}")
+
+        case statement[0].source
+        when 'description'
+          cookbook_obj.short_desc = statement.parameters.first.jump(:string_content).source
+        when 'version'
+          cookbook_obj.version = statement.parameters.first.jump(:string_content).source
+        end
+      end
+    end
+  end
+end
