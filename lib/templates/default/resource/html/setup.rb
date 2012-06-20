36057f1af3e70d202be3a91e3a09b43a56d31d6f
Nitin Mohan <nitin@rightscale.com>
2012-06-20 17:54:30 +0000

    Fixed links in templates for resource page.

--- lib/templates/default/resource/html/setup.rb
+++ lib/templates/default/resource/html/setup.rb
@@ -2,5 +2,5 @@ include T('default/fulldoc/html')
 
 def init
   sections.push :title
-  sections.push :resource_name, [:action_table, :attribute_table, :providers]
+  sections.push :resource_name, [:action_table, :attribute_table, :providers], :end
 end
