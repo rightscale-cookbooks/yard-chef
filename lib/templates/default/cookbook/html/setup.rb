6cbaad440adcebcd58dcdea189f1a99aaf14432c
Nitin Mohan <nitin@rightscale.com>
2012-06-20 17:50:09 +0000

    Added templates for displaying cookbook components

--- lib/templates/default/cookbook/html/setup.rb
+++ lib/templates/default/cookbook/html/setup.rb
@@ -2,9 +2,36 @@ include T('default/fulldoc/html')
 
 def init
   sections.push :cookbook_title, [:cookbook_desc]
-  sections.push :recipes_title, [:recipes_table]
-  sections.push :attributes_title, [:attributes_table]
-  sections.push :resources_title, [:resource_list]
-  #sections.push :definitions_title, [:definition_list]
-  #sections.push :libraries_title, [:library_list]
+  sections.push :recipes, :object_title, [:recipes_table]
+  sections.push :attributes, :object_title, [:attributes_table]
+  sections.push :resources, :object_title, [:object_list]
+  sections.push :definitions, :object_title, [:definitions_list]
+  sections.push :libraries, :object_title, [:library_list]
+
+  @libraries = YARD::Registry.all(:module)
+end
+
+def component_name(object)
+  case object.class.to_s
+    when 'YARD::CodeObjects::Chef::RecipeObject'
+      'Recipes'
+    when 'YARD::CodeObjects::Chef::AttributeObject'
+      'Attributes'
+    when 'YARD::CodeObjects::Chef::ResourceObject'
+      'Resources'
+    when 'YARD::CodeObjects::Chef::DefinitionObject'
+      'Definitions'
+    when 'String'
+      'Libraries'
+  end
+end
+
+def find_library(library_file)
+  libs = []
+  @libraries.each do |library|
+    if library.file == library_file
+      libs.push(library)
+    end
+  end
+  libs
 end
