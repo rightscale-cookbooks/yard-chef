556d5a5c5147c72d88c11d0c8bef0388833d764d
Nitin Mohan <nitin@rightscale.com>
2012-06-21 00:56:59 +0000

    * Removed dependency on fulldoc * Rearranged sections * Removed unwanted methods

--- lib/templates/default/cookbook/html/setup.rb
+++ lib/templates/default/cookbook/html/setup.rb
@@ -1,29 +1,12 @@
-include T('default/fulldoc/html')
-
 def init
   sections.push :cookbook_title, [:cookbook_desc]
-  sections.push :recipes, :object_title, [:recipes_table]
-  sections.push :attributes, :object_title, [:attributes_table]
-  sections.push :resources, :object_title, [:object_list]
-  sections.push :definitions, :object_title, [:definitions_list]
-  sections.push :libraries, :object_title, [:library_list]
+  sections.push :recipes, [:recipes_table]
+  sections.push :attributes, [:attributes_table]
+  sections.push :resources, [:resource_list]
+  sections.push :definitions, [:definitions_list]
+  sections.push :libraries, [:library_list]
 
   @libraries = YARD::Registry.all(:module)
-end
-
-def component_name(object)
-  case object.class.to_s
-    when 'YARD::CodeObjects::Chef::RecipeObject'
-      'Recipes'
-    when 'YARD::CodeObjects::Chef::AttributeObject'
-      'Attributes'
-    when 'YARD::CodeObjects::Chef::ResourceObject'
-      'Resources'
-    when 'YARD::CodeObjects::Chef::DefinitionObject'
-      'Definitions'
-    when 'String'
-      'Libraries'
-  end
 end
 
 def find_library(library_file)
