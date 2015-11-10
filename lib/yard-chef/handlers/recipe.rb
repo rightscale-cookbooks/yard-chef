5b5792ac02ea284f2f2796f7c2a3547fa46cf9cd
Aleksey Hariton <aleksey.hariton@gmail.com>
2015-11-10 17:46:57 +0300

    Recipe comments

--- lib/yard-chef/handlers/recipe.rb
+++ lib/yard-chef/handlers/recipe.rb
@@ -32,7 +32,7 @@ module YARD::Handlers
         path_array = statement.file.to_s.split('/')
 
         # Recipe declaration in metadata.rb
-        if path_array.include? 'metadata.rb' && statement.jump(:ident) == 'recipe'
+        if path_array.include? 'metadata.rb' && statement.jump(:ident).source.match(/recipe/)
           description = ''
           recipe_obj = ChefObject.register(cookbook, name, :recipe)
           # YARD builds an abstract syntax tree (AST) which we need to traverse
